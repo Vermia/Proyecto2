b97f266c8d727043b76a1ed3ef6bc11101d87c1f
PabloMendezMayoral10 <73696353+PabloMendezMayoral10@users.noreply.github.com>
2022-11-22 17:33:34 +0100

    He realizado cambios en el proyecto

--- LucaSteamProyecto2/src/main/java/com/proyecto2/spring/repository/VideojuegoBBDD.java
+++ LucaSteamProyecto2/src/main/java/com/proyecto2/spring/repository/VideojuegoBBDD.java
@@ -1,11 +1,19 @@
 package com.proyecto2.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.proyecto2.spring.model.Videojuego;
 
+
+
 @Repository
 public interface VideojuegoBBDD extends JpaRepository<Videojuego, Integer>{
-	
+	List<Videojuego> findByNombre(String nombre);
+		
+	@Query("FROM videojuegos WHERE nombre =?1")
+	List<Videojuego> buscarPorNombre(String nombre);
 }
--- LucaSteamProyecto2/src/test/java/com/proyecto2/spring/LucaSteamProyecto2ApplicationTests.java
+++ LucaSteamProyecto2/src/test/java/com/proyecto2/spring/LucaSteamProyecto2ApplicationTests.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.proyecto2.spring.model.Videojuego;
@@ -12,6 +13,7 @@ import com.proyecto2.spring.repository.VideojuegoRepository;
 
 @SpringBootTest
 class LucaSteamProyecto2ApplicationTests {
+	@Autowired
 	VideojuegoRepository vidRep;
 
 	@Test
