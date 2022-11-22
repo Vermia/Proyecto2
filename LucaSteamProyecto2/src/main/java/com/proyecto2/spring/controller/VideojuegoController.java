0b7a4c7f0d40ba33fc166021aceee8b53dac1eef
Ioan Stirbu <55089021+IOANSTIRBU@users.noreply.github.com>
2022-11-22 13:04:34 +0100

    Añadido JPA

--- LucaSteamProyecto2/src/main/java/com/proyecto2/spring/controller/VideojuegoController.java
+++ LucaSteamProyecto2/src/main/java/com/proyecto2/spring/controller/VideojuegoController.java
@@ -1,5 +1,7 @@
 package com.proyecto2.spring.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.proyecto2.spring.model.Videojuego;
 import com.proyecto2.spring.service.VideojuegoService;
 
 @RestController
@@ -19,5 +22,8 @@ public class VideojuegoController {
 	private static final Logger logger = LoggerFactory.getLogger(VideojuegoController.class);
 	
 	@GetMapping
-	public List
+	public List<Videojuego>cargaJuegos(){
+		return null;
+		
+	}
 }
