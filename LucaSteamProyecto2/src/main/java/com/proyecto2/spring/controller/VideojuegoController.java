d9221c5a6f0fb21ed534818f0093f43ee1046f7c
Ioan Stirbu <55089021+IOANSTIRBU@users.noreply.github.com>
2022-11-22 13:28:05 +0100

    Añadido en controller altaJuegos

--- LucaSteamProyecto2/src/main/java/com/proyecto2/spring/controller/VideojuegoController.java
+++ LucaSteamProyecto2/src/main/java/com/proyecto2/spring/controller/VideojuegoController.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +32,11 @@ public class VideojuegoController {
 		return null;
 		
 	}
+	
+	@PostMapping
+	public void altaJuegos(@RequestBody Videojuego juego) {
+		service.save(juego);
+	}
+	
+	
 }
