80ce594d3df4f6924f10908fa358fe514184e29e
Vermia <domeofvermia@outlook.com>
2022-11-22 16:41:14 +0100

    prueba unitaria

--- LucaSteamProyecto2/src/test/java/com/proyecto2/spring/LucaSteamProyecto2ApplicationTests.java
+++ LucaSteamProyecto2/src/test/java/com/proyecto2/spring/LucaSteamProyecto2ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.proyecto2.spring;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.proyecto2.spring.model.Videojuego;
+import com.proyecto2.spring.repository.VideojuegoRepository;
+
 @SpringBootTest
 class LucaSteamProyecto2ApplicationTests {
+	VideojuegoRepository vidRep;
 
 	@Test
-	void contextLoads() {
+	void testListaNoVacia() {
+		ArrayList<Videojuego> lista = (ArrayList<Videojuego>) vidRep.cargaJuegos();
+		
+		assertThat(lista).isNotEmpty();
 	}
 
 }
