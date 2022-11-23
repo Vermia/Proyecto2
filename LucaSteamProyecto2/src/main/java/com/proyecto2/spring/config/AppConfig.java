ab72bf495c2a9a6a9cb2f7d3781fd1ded98f1a1e
Ioan Stirbu <55089021+IOANSTIRBU@users.noreply.github.com>
2022-11-23 17:19:13 +0100

    update swagger

--- LucaSteamProyecto2/src/main/java/com/proyecto2/spring/config/AppConfig.java
+++ LucaSteamProyecto2/src/main/java/com/proyecto2/spring/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.proyecto2.spring.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -9,7 +10,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 
-
+@Configuration
 public class AppConfig {
 	/**
 	 * 
